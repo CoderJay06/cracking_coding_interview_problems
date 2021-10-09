881b226e71f37ccdc6612b66fb6cb4622594e06b
Jay Cruz <49811999+CoderJay06@users.noreply.github.com>
2021-10-09 06:33:27 +0000

    Modify string compresssion

--- Arrays and Strings/string_compression.js
+++ Arrays and Strings/string_compression.js
@@ -44,6 +44,7 @@ function stringCompression(str) {
     return compressedStr.join('');
 }
 
+// O(p + k^2)
 function stringCompressBad(str) {
     let compressedString = "";
     let countConsecutive = 0;
@@ -59,7 +60,25 @@ function stringCompressBad(str) {
     return compressedString.length < str.length ? compressedString : str;
 }
 
+function compress(str) {
+    const compressed = [];
+    let countConsecutive = 0;
+    for (let i = 0; i < str.length; i++) {
+        countConsecutive++;
+
+        // If next char is different than current, append it to compressed
+        if (i + 1 >= str.length || str.charAt(i) !== str.charAt(i + 1)) {
+            compressed.push(str.charAt(i));
+            compressed.push(countConsecutive);
+            countConsecutive = 0;
+        }
+    }
+    return compressed.length < str.length ? compressed.join('') : str;
+}
+
 
 console.log(stringCompression('aabcccccaaa')); // a2b1c5a3
 
 console.log(stringCompressBad('aabcccccaaa'));
+
+console.log(compress('aabcccccaaa'));
