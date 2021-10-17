ca3be3d77b977be1d1ca8e00d0f2ecfbdf2d972c
Jay Cruz <49811999+CoderJay06@users.noreply.github.com>
2021-10-17 06:34:56 +0000

    Modify linnked list files

--- Linked Lists/create_linked_list.js
+++ Linked Lists/create_linked_list.js
@@ -38,3 +38,4 @@ class Node {
 }
 
 
+export default Node;
---
+++ Linked Lists/remove_dups.js
@@ -0,0 +1,9 @@
+/*
+    Problem: Write code to remove duplicates from an unsorted linked list.
+
+    How would you solve this problem if a temporary buffer is not allowed? 
+*/
+
+const llist = new Node(6);
+
+console.log(llist)
